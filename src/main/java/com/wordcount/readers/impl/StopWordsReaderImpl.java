922053f625ab92e51e6b53e3991fea04dd82da6a
Natalija Rozok <natalija.rozok@applifting.czk>
2021-03-23 10:12:46 +0100

    FIX: Fix bug in creation of empty List

---
+++ src/main/java/com/wordcount/readers/impl/StopWordsReaderImpl.java
@@ -0,0 +1,42 @@
+package com.wordcount.readers.impl;
+
+import com.wordcount.domain.WordCounter;
+import com.wordcount.readers.StopWordsReader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StopWordsReaderImpl implements StopWordsReader {
+
+    private final String STOP_WORDS_FILE_NAME = "stopwords.txt";
+
+    @Override
+    public List<String> read() {
+
+        List<String> stopWords = new ArrayList<>();
+        String line;
+
+        try {
+            BufferedReader bufferedReader = getStopWordsFilerReader();
+            while ((line = bufferedReader.readLine()) != null) {
+                stopWords.add(line);
+            }
+        } catch (Exception e) {
+            String errorMessage = e.getMessage();
+        }
+        ;
+
+        return stopWords;
+    }
+
+    private BufferedReader getStopWordsFilerReader() throws FileNotFoundException {
+        URL fileUrl = WordCounter.class.getClassLoader().getResource(STOP_WORDS_FILE_NAME);
+        File file = new File(fileUrl.getFile());
+        return new BufferedReader(new FileReader(file));
+    }
+}
